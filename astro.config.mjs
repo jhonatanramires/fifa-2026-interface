33ca1e10c17a432877ec553e13130091003e713e
jhonatanramires <adm.jhonatanramirez@gmail.com>
2026-06-21 21:43:26 -0500

    add @astrojs/ sitemap for sitemap for the google seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
+  site: 'https://example.com',
   server: {
     host: true,
   },
+
+  integrations: [sitemap()],
 })
