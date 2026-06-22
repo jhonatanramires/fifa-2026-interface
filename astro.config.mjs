b96362a9b564315bdc85a8a20d3823764fb7ee31
jhonatanramires <adm.jhonatanramirez@gmail.com>
2026-06-22 14:54:48 -0500

    adding astro-icon to replace font-awesome for google page performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config"
 
 import sitemap from "@astrojs/sitemap";
 
+import icon from "astro-icon";
+
 export default defineConfig({
   site: 'https://example.com',
   server: {
     host: true,
   },
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
 })
