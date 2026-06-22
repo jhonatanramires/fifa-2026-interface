dea67d988eb464485df627faf57911a2340e03ba
jhonatanramires <adm.jhonatanramirez@gmail.com>
2026-06-22 15:13:11 -0500

    optimizing fonts and refining layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,13 @@
-import { defineConfig, fontProviders } from "astro/config"
+import { defineConfig } from "astro/config"
 
 import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
 export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.google(), // o fontProviders.fontsource()
-      name: "Inter",
-      cssVariable: "--font-inter", // variable que usarás en CSS
-      weights: [400, 600, 700, 800], // los pesos que necesitas
-      styles: ["normal"],
-      // opcional: subsets, fallbacks, etc.
-    },
-  ],
-  site: 'https://example.com',
+  site: 'https://distripanini.com',
   server: {
     host: true,
   },
-
   integrations: [sitemap(), icon()],
 })
